bd0a89f09cacc0454a5c97c74bff27bf8d8c0f09
nic-jol <njlefebvre@gmail.com>
2018-03-08 21:35:43 -0700

    Change nickname and color fully working; checks repeat names

--- index.js
+++ index.js
@@ -11,22 +11,24 @@ const port = 3000;    // Port constant used throughout
 
 // Users object
 var allUsers = {};   // store all the user names & colors
- //key = username; value = color
+//key = username; value = color
 
-const colors = ["Aqua", "Blue", "Blue Violet", "Brown", "Coral", "CornflowerBlue", "Crimson", "DarkGoldenRod", "DarkGrey", "DarkGreen", "DarkMagenta", "DeepPink", "Fuschia", "Gold", "LightGreen"]; // 15 elem
+const colors = ["Aqua", "Blue", "BlueViolet", "Brown", "Coral", "CornflowerBlue", "Crimson", "DarkGoldenRod", "DarkGrey", "DarkGreen", "DarkMagenta", "DeepPink", "Gold", "LightGreen"]; // 14 elem
 const name1 = ["crazy", "docile", "mellow", "hyper", "chipper", "goofy", "haughty", "envious", "cute", "hungry"]; //10 elem
 const name2 = ["Squirrel", "Hippo", "Bobcat", "Rhinocerous", "Ibex", "Axolotl", "Dingo", "Hyena", "Capybara", "RockHyrax"];  // 10 elem
+//const name1 = ["crazy", "docile"]; //10 elem
+//const name2 = ["Squirrel", "Hippo"];  // 10 elem
 
 
 //make the server listen on port 3000
-server.listen(port, function(){
+server.listen(port, function () {
     console.log('Server running on port ' + port);
 });
 
 
 
 //When they request this on our server, send index.html
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
     // Capital F - not deprecated
     res.sendFile(__dirname + '/index.html');
 });
@@ -47,30 +49,36 @@ io.on('connection', function(socket){
 });
 */
 // Event: connection
-io.on('connection', function(socket) {
+io.on('connection', function (socket) {
     console.log("user connected");
-    
-    let user = {name:"", color:""};
+
+    let user = { name: "", color: "" };
 
     // Check cookies for username and color
     if (false) {
 
-    } 
+    }
     // Or randomly generate
     else {
         // Generate name
-        let num1 = Math.round(Math.random() * 9);
-        let num2 = Math.round(Math.random() * 9);
+        let genName = "";
 
-        console.log("Num1: " + num1);
-        console.log("Num2: " + num2);
+        do {   // Make sure it isn't aldready in allUsers
+            let num1 = Math.round(Math.random() * 9);
+            let num2 = Math.round(Math.random()* 9);
 
-        let genName = "" + name1[num1] + name2[num2];
+            //console.log("Num1: " + num1);
+            //console.log("Num2: " + num2);
+
+            genName = "" + name1[num1] + name2[num2];
+            console.log(genName);
+            console.log(allUsers);
+        } while (genName in allUsers);
 
         // Generate color
-        let num3 = Math.round(Math.random() * 14);
+        let num3 = Math.round(Math.random() * 13);
 
-        console.log("Num3: " + num3);
+        //console.log("Num3: " + num3);
 
         let genColor = colors[num3];
 
@@ -100,43 +108,79 @@ io.on('connection', function(socket) {
         let rightTime = withSeconds[0] + ":" + withSeconds[1];
 
         // Parse message
-    
-    
+
+
         // Change nickname or change in color requested/nick <new nickname>
         let possibleRequest = msg.split(" ");
 
         if (possibleRequest[0].trim() == "/nick") {
             console.log("change name request");
             let oldname = user["name"];
-            // Todo: check array length
-            user["name"] = possibleRequest[1].trim();
-            console.log(user);
 
-            io.emit('message', (rightTime + " " + oldname + " changed their name to " + user["name"]) + ".");
+            if ((possibleRequest.length > 1) && !(possibleRequest[1].trim() in allUsers)) { // Make sure there is a name to change it to and that there isn't already a user with that name
+                user["name"] = possibleRequest[1].trim();
+                console.log(user);
+
+                io.emit('message', ("<li>" + rightTime + " " + oldname + " changed their name to " + "<span style=\"color: " + user["color"] + ";\">" + user["name"] + "</span>" + "." + "</li>"));
+            }
+            else {
+                io.emit('message', ("<li>" + rightTime + " " + "<span style=\"color: " + user["color"] + ";\">" + user["name"] + "</span>" + "'s name change not successful" + "</li>"));
+            }
+
         }
         else if (possibleRequest[0].trim() == "/nickcolor") {
             console.log("change color request");
-            user["color"] = possibleRequest[1].trim();
-            console.log(user);
+            if ((possibleRequest.length > 1) && (possibleRequest[1].length == 6) && (all0ToF(possibleRequest[1]))) {
+                user["color"] = "#" + possibleRequest[1].trim();
+                console.log(user);
 
-            // TODO: separate self and others
-            io.emit('message', (rightTime + " " + user["name"] + " changed color."));
-        } 
+                // TODO: separate self and others
+                io.emit('message', ("<li>" + rightTime + " " + "<span style=\"color: " + user["color"] + ";\">" + user["name"] + "</span>" + " changed color." + "</li>"));
+            }
+            else {
+                io.emit('message', ("<li>" + rightTime + " " + "<span style=\"color: " + user["color"] + ";\">" + user["name"] + "</span>" + "'s color change not successful" + "</li>"));
+            }
+
+        }
         else {
             console.log(`message ${msg}`);
-            io.emit('message', (rightTime + " " + user["name"]+ ": " + msg));
+            io.emit('message', ("<li>" + rightTime + " " + "<span style=\"color: " + user["color"] + ";\">" + user["name"] + "</span>" + ": " + msg + "</li>"));
         }
     });
 
     socket.on('disconnect', () => {
+        // timestamp
+        let fullDate = Date().split(" ");
+        let withSeconds = fullDate[4].split(':');
+        let rightTime = withSeconds[0] + ":" + withSeconds[1];
+
+        // Remove nickname from allUsers
+
+        // Better message
         console.log("user disconnected");
+        io.emit('message', ("<li>" + rightTime + " " + "<span style=\"color: " + user["color"] + ";\">" + user["name"] + "</span>" + " has left the chat room." + "</li>"));
+
+
 
         // use io since server - emitting from server (io)
-        io.emit('message', 'user disconnected');
+        //io.emit('message', 'user disconnected');
     })
-    
+
 });
-    
+
+function all0ToF(str) {
+    str = str.toLowerCase();
+    for (let i= 0; i < 6; i++) {
+        let letterCode = str.charCodeAt(i);
+
+        if ((letterCode < 48) || (letterCode > 57 && letterCode < 97) || (letterCode > 102)) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
 /*    
     // Send to all
     console.log('a user connected');
